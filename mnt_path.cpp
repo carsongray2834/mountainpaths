1dad3a81035078196859df06cf28fb5315a236dd
fabio_maldonado02 <fabianmaldonado123678@gmail.com>
2025-04-08 20:10:57 -0700

    Added a couple of commments FM Tue Apr 8 8:10pm

--- mnt_path.cpp
+++ mnt_path.cpp
@@ -6,6 +6,9 @@
 #include <sstream>
 #include <string>
 #include <limits>
+//Added for Abs - FM Mon Apr 7 10:38 PM
+#include <cmath>
+#include <cstdlib>
 
 #include "DataSource.h"
 #include "data_src/ElevationData.h"
@@ -50,16 +53,23 @@ void findPath(const ElevationData&  elev_data, int startRow, ColorGrid& cg) {
 	//
 	// always move right, but select the right cell, the top right cell, or bottom right cell
 	// by minimizing the difference of elevation.
-	//
+	//Note FM Tue Apr 8 7:58 PM
+	// I watch a couple of videos on Dijkstra's Algorithm
+	// I noticed cg doesn't have a get x or y method but they do have a getWidth and getRow(), That could be something we need to use
+	// End of Note
 	// Write path to the colorgrid
 	int x = 0;
 	int y = startRow;
-
+	//int top_right = 
+	//cout << cg.getWidth() << endl;
+	//Commented this out  didn't work int top_right = abs(cg.get(y+1,x+1) - cg.get(y,x));
 	while (x < cg.getWidth()) {
-		cg.get(y+1,x+1);
-
+		//cg.get(y,x);
+         
+	//int top_right = cg.get(((y+1)-y),((x+1)-x));
 		cg.set(y, x, Color(255, 0, 0));
 		x++;
+//		y-=1;
 	}
 }
 
@@ -69,7 +79,7 @@ int main(int argc, char **argv) {
 
 
 	// initialize Bridges
-	Bridges bridges(123, "ID", "api key");
+	Bridges bridges(123, "ID_USER", "API_KEY");
 
 	// defaults for row number and data file
 	int startRow = 50;
@@ -79,7 +89,8 @@ int main(int argc, char **argv) {
 	bridges.setDescription("Your goal is to find a path that takes you through the points with the lowest elevation changes, in an effort to minimize the overall effort in walking through the path.");
 
 	// get elevation data
-
+    // Notes FM Mon Apr 7 11:42 PM 
+	// ElevationData (int cols, int rows, int xll, int yll, float cellsize, int maxVal, int minVal)
 	DataSource ds;
 	ElevationData elev_data = ds.getElevationData(6.02, 44.10, 9.70, 47.77, 0.02);
 
